de2679aaaf30eff98a0ef0bc0ac1db3889a42a3c
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-12-25 14:52:02 +0100

    fix: fix prettier not working correctly with lint --fix ( fix #410)

--- app.config.ts
+++ app.config.ts
@@ -80,7 +80,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     'expo-localization',
     'expo-router',
     ['app-icon-badge', appIconBadgeConfig],
-    ["react-native-edge-to-edge"]
+    ['react-native-edge-to-edge'],
   ],
   extra: {
     ...ClientEnv,
