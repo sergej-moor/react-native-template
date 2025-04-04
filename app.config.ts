446821a282f2af8cb53674c628ad72b6a2383e75
Lucas Raposeiras <raposeiras.lucas@gmail.com>
2025-04-04 14:20:21 -0300

    feat: EAS build with GitHub Actions

--- app.config.ts
+++ app.config.ts
@@ -30,6 +30,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: Env.BUNDLE_ID,
+    config: {
+      usesNonExemptEncryption: false,
+    },
   },
   experiments: {
     typedRoutes: true,
